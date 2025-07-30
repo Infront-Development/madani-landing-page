fa996842861e4ae8edc744743995877ef8350454
NiyazInfront <mohamad.niyaz@infrontconsulting.com>
2025-07-30 16:16:29 +0800

    Progress 1

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import frame4 from './assets/Frame_4.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Frame-7">
+        <div className='Frame-5'>
+          <div className='Frame-9'>
+            <div className='Frame-3'>
+              <span className="Digitalise-your-business-with-up-to">
+                Digitalise your business
+                <span className="text-style-1">with up to</span>
+              </span>
+              <span className="RM5000-Grant">
+                RM5000
+                <span className="text-style-1"></span>
+                <span className="text-style-2">Grant</span>
+              </span>
+
+            </div>
+          </div>
+          <div className='Frame-4'><img src={frame4} alt="Frame 4"></img></div>
+        </div>
+      </div>
     </div>
   );
 }
